c7f42efd66eaab8daa72be669a7f9c0f5bf0b472
bishal_0x01 <bishal.bhowmik@blackbuck.com>
2026-08-27 18:35:35 +0530

    added embedding tutorials

--- springAI/chatApp/src/main/java/com/springAI/chatApp/services/AIService.java
+++ springAI/chatApp/src/main/java/com/springAI/chatApp/services/AIService.java
@@ -6,10 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -19,8 +23,24 @@ public class AIService {
     @Autowired
     private ChatClient chatClient;
 
-
     private final EmbeddingModel embeddingModel;
+
+    private final VectorStore vectorStore;
+
+    public List<Document> similaritySearch(String text){
+        return vectorStore.similaritySearch(SearchRequest.builder()
+                        .query(text)
+                        .topK(3)
+                        .similarityThreshold(0.3)
+                .build());
+    }
+
+    public void ingestDataToVectorStore(String text){
+
+        Document document = new Document(text);
+        vectorStore.add(List.of(document));
+
+    }
 
     public float[] getEmbedding(String text){
         return embeddingModel.embed(text);
--- springAI/chatApp/src/test/java/com/springAI/chatApp/service/AIServiceTest.java
+++ springAI/chatApp/src/test/java/com/springAI/chatApp/service/AIServiceTest.java
@@ -2,8 +2,11 @@ package com.springAI.chatApp.service;
 
 import com.springAI.chatApp.services.AIService;
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.document.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class AIServiceTest {
@@ -15,6 +18,31 @@ public class AIServiceTest {
     public void testGetJoke(){
         var joke = aiService.getJoke("Cats");
         System.out.println(joke);
+    }
+
+
+    @Test
+    public void testEmbedding(){
+        var embed = aiService.getEmbedding("This is a big text");
+
+        System.out.println("embed-length : " + embed.length);
+
+        for(float e : embed){
+            System.out.print(e + " ");
+        }
+    }
+
+    @Test
+    public void testStoreData(){
+        aiService.ingestDataToVectorStore("Apple is nice fruit.");
+    }
+
+    @Test
+    public void testSimilaritySearch(){
+        var res =  aiService.similaritySearch("big text.");
+        for(var doc: res){
+            System.out.println(doc);
+        }
     }
 
 }
